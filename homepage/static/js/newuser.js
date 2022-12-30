72d940aeaf3dd700683a0c4760b61dd210a34d20
Younus-Saberi <younussaberi@gmail.com>
2022-12-30 21:36:22 +0530

    linked JS file

---
+++ homepage/static/js/newuser.js
@@ -0,0 +1,9 @@
+ // function for validation of register form
+ function validateRegister() {
+    alert("Submit Button was clicked on register form");
+  }
+
+  // function for validation of login form
+  function validateLoginForm() {
+    alert("Login Button was clicked on login form");
+  }
