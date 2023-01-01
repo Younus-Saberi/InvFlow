9c200c403775576b26952530862fbe91f2386706
Younus-Saberi <younussaberi@gmail.com>
2023-01-01 15:42:48 +0530

    rm static route

--- core/urls.py
+++ core/urls.py
@@ -29,5 +29,3 @@ urlpatterns = [
     path('transactions/', include('transactions.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
